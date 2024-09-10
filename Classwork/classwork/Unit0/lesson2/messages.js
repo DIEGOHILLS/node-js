87b2e3956271f2fa15ba3becb6cf8f20ce0e1a1e
DIEGOHILLS <125441047+DIEGOHILLS@users.noreply.github.com>
2024-09-10 18:35:37 +0200

    node

---
+++ Classwork/classwork/Unit0/lesson2/messages.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let messages = [
+    "From HTMX to NodeJS", 
+    "You will make it!",
+    "Just run with the code!"
+];
+
+
+
+
+
+
+
---
+++ Classwork/classwork/Unit1/lesson3/messages.js
@@ -0,0 +1,16 @@
+"use strict";
+//Export makes file available to other files
+exports.messages = [
+    "Keep on going!", 
+    "You can accomplish anything",
+    "Success is in your future!"
+];
+
+//npm install [-Dev] <dependency> --save
+//npm init -y 
+
+
+
+
+
+
